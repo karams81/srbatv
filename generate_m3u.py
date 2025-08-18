ae38f292f4ac00c5da37f2b3740298f2b678d3a8
ahmet21ahmet <53349631+ahmet21ahmet@users.noreply.github.com>
2025-08-18 19:27:01 +0300

    Update generate_m3u.py

--- generate_m3u.py
+++ generate_m3u.py
@@ -1,11 +1,11 @@
 import requests
 import os
 
-BASE_URL = "https://www.atv.com.tr/kurulus-osman/"
+BASE_URL = "https://www.atv.com.tr/karadayi"
 PROXY_PREFIX = "https://stream-extractor.koprulu.workers.dev/?url="
 EXT = "&ext=mp4"
-COVER_ART = "https://iaatv.tmgrup.com.tr/71709e/500/268/0/0/500/268?u=https://iatv.tmgrup.com.tr/2024/09/24/500x268/1727165994263.jpg"
-CATEGORY = "Kuruluş Osman"
+COVER_ART = "https://iaatv.tmgrup.com.tr/3b3f5e/500/268/0/0/500/268?u=https://iatv.tmgrup.com.tr/2021/06/24/500x268/1624539164040.jpg"
+CATEGORY = "Kara Dayı"
 USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
 
 def get_last_episode():
@@ -28,10 +28,10 @@ def generate_m3u(end_episode):
 
     for ep in range(1, end_episode + 1):
         stream_url = f"{PROXY_PREFIX}{BASE_URL}{ep}-bolum/izle{EXT}"
-        m3u += f"""#EXTINF:-1 tvg-id="KurulusOsman{ep}" tvg-name="Bölüm-{ep}" tvg-logo="{COVER_ART}" group-title="{CATEGORY}",Kuruluş Osman Bölüm-{ep}
+        m3u += f"""#EXTINF:-1 tvg-id="karadayı{ep}" tvg-name="Bölüm-{ep}" tvg-logo="{COVER_ART}" group-title="{CATEGORY}",Kara Dayı Bölüm-{ep}
 {stream_url}\n"""
 
-    with open("kurulus-osman.m3u", "w", encoding="utf-8") as f:
+    with open("kara_dayı.m3u", "w", encoding="utf-8") as f:
         f.write(m3u)
 
 if __name__ == "__main__":
